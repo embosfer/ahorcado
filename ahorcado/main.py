3a628f3f421ab17d65dee46506eb49be9c2d8f3f
embosfer <e.bosch.ferrando@gmail.com>
2018-03-25 21:40:52 +0100

    Print hangman as well

--- ahorcado/main.py
+++ ahorcado/main.py
@@ -25,15 +25,39 @@ class Ahorcado:
         self.letters_used = []
         self.strikes = 0
         self.letters_left = len(word_to_guess)
+        self.hangman = ['' for _ in range(6)]
 
     def __str__(self):
-        return ' '.join(self.current_word) + '\n\n' + ','.join(self.letters_used) + '\n'
+        return ''.join(reversed(self.hangman)) + '\n\n' + ' '.join(self.current_word) + '\n\n' + ','.join(self.letters_used) + '\n'
+
+    def refresh_hangman(self):
+        if self.strikes == 1:
+            self.hangman[0] = '\t\t\t_|_\n'
+        elif self.strikes == 2:
+            self.hangman[1] = '\t\t\t |\n'
+        elif self.strikes == 3:
+            self.hangman[2] = '\t\t\t |\n'
+        elif self.strikes == 4:
+            self.hangman[3] = '\t\t\t |\n'
+        elif self.strikes == 5:
+            self.hangman[4] = '\t\t\t |\n'
+        elif self.strikes == 6:
+            self.hangman[5] = '\t\t\t  ____\n'
+        elif self.strikes == 7:
+            self.hangman[4] = self.hangman[4][:-1] + '    |\n'
+        elif self.strikes == 8:
+            self.hangman[3] = self.hangman[3][:-1] + '    O\n'
+        elif self.strikes == 9:
+            self.hangman[2] = self.hangman[2][:-1] + '   -|- \n'
+        elif self.strikes == 10:
+            self.hangman[1] = self.hangman[1][:-1] + '   / \ \n'
 
     def play(self):
         while not self.hung() and not self.freed():
             print(self)
-            letter = input('Which letter? ')
+            letter = input('Which letter? \n\n')
             self.check_ifexists(letter)
+            self.refresh_hangman()
 
         print(self)
         self.print_finalresult()
@@ -47,7 +71,7 @@ class Ahorcado:
             print('Well done!')
 
     def hung(self):
-        return self.strikes == 9
+        return self.strikes == 10
 
     def freed(self):
         return self.letters_left == 0
